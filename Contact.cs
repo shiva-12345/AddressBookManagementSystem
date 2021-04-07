24cd75b8a3e84e982a48f82dd9b7c9eba32fac62
shiva-12345 <munnoorushiva07@gmail.com>
2021-04-07 20:42:33 +0530

    contact added

---
+++ Contact.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class Contacts
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
+
+
+    }
+}
