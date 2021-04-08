a0bb7de200529e9e92b82830ded9adac57837689
shiva-12345 <munnoorushiva07@gmail.com>
2021-04-08 15:30:33 +0530

    add multiple records

--- AddressBook.cs
+++ AddressBook.cs
@@ -38,10 +38,9 @@ namespace AddressBookSystem
             book.Add(newRecord);
             Console.WriteLine("Record added to address book " + bookName);
             Console.WriteLine("Your Data :\n" + newRecord.toString());
+            Console.WriteLine();
 
         }
-
-
         // initialize book if not present allready
         public static void addBook(string bookName)
         {
@@ -53,7 +52,7 @@ namespace AddressBookSystem
             contactDetails(bookName);
         }
 
-        public static void editRecordUsingFisrtName(string recordNameToEdit, string bookToEdit)
+        public static void editRecordUsingFisrtName(string bookToEdit, string recordNameToEdit)
         {
 
             if (Program.addressBookStore.ContainsKey(bookToEdit))
@@ -65,8 +64,9 @@ namespace AddressBookSystem
                     {
                         if (record.first_name.Equals(recordNameToEdit))
                         {
+
                             Console.WriteLine("Select Which Data You Want To Update \n1.First_Name \n2.Last_Name \n3.Address" +
-                                                   "\n4.City \n5.State \n6.Zip \n7.PhoneNumber \n8.Email");
+                                               "\n4.City \n5.State \n6.Zip \n7.PhoneNumber \n8.Email");
                             string selection = Console.ReadLine();
                             string newData;
                             switch (selection)
@@ -76,56 +76,56 @@ namespace AddressBookSystem
                                     newData = Console.ReadLine();
                                     record.first_name = newData;
                                     Console.WriteLine("Your Updated  Records :\n" + record.toString());
-
+                                    Console.WriteLine();
                                     break;
                                 case "2":
                                     Console.WriteLine("Enter Last_Name");
                                     newData = Console.ReadLine();
                                     record.last_name = newData;
                                     Console.WriteLine("Your Updated  Records :\n" + record.toString());
-
+                                    Console.WriteLine();
                                     break;
                                 case "3":
                                     Console.WriteLine("Enter Address");
                                     newData = Console.ReadLine();
                                     record.address = newData;
                                     Console.WriteLine("Your Updated  Records :\n" + record.toString());
-
+                                    Console.WriteLine();
                                     break;
                                 case "4":
                                     Console.WriteLine("Enter City");
                                     newData = Console.ReadLine();
                                     record.city = newData;
                                     Console.WriteLine("Your Updated  Records :\n" + record.toString());
-
+                                    Console.WriteLine();
                                     break;
                                 case "5":
                                     Console.WriteLine("Enter State");
                                     newData = Console.ReadLine();
                                     record.state = newData;
                                     Console.WriteLine("Your Updated  Records :\n" + record.toString());
-
+                                    Console.WriteLine();
                                     break;
                                 case "6":
                                     Console.WriteLine("Enter Zip");
                                     newData = Console.ReadLine();
                                     record.zip = newData;
                                     Console.WriteLine("Your Updated  Records :\n" + record.toString());
-
+                                    Console.WriteLine();
                                     break;
                                 case "7":
                                     Console.WriteLine("Enter PhoneNumber");
                                     newData = Console.ReadLine();
                                     record.phoneNumber = newData;
                                     Console.WriteLine("Your Updated  Records :\n" + record.toString());
-
+                                    Console.WriteLine();
                                     break;
                                 case "8":
                                     Console.WriteLine("Enter Email");
                                     newData = Console.ReadLine();
                                     record.email = newData;
                                     Console.WriteLine("Your Updated  Records :\n" + record.toString());
-
+                                    Console.WriteLine();
                                     break;
                                 default:
                                     Console.WriteLine("Invalid Selection Input");
@@ -133,10 +133,9 @@ namespace AddressBookSystem
                             }
                             break;
                         }
-                    }
-                    else
-                    {
-                        Console.WriteLine("First_Name: " + record.first_name + " not Exist");
+                        {
+                            Console.WriteLine("First_Name: " + record.first_name + " not Exist");
+                        }
                     }
                 }
             }
@@ -161,6 +160,11 @@ namespace AddressBookSystem
                             Console.WriteLine("Your Record Deleted :");
                             break;
                         }
+                        else
+                        {
+                            Console.WriteLine("Your Record not found!");
+                            break;
+                        }
 
                     }
                 }
@@ -175,4 +179,4 @@ namespace AddressBookSystem
 
 
     }
-}s
+}
--- Program.cs
+++ Program.cs
@@ -11,14 +11,44 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Wellcome to address book system program !");
-            Console.WriteLine("Enter name of the address book in which you want to add record.");
-            String bookName = Console.ReadLine();
-            AddressBook.addRecord(bookName);
-            Console.WriteLine("Enter the name fo the address book you want to edit.");
-            String bookToEdit = Console.ReadLine();
-            Console.WriteLine("Enter the first name of the person whose record you want to edit.");
-            String recordNameToEdit = Console.ReadLine();
-            AddressBook.editRecordUsingFisrtName(recordNameToEdit, bookToEdit);
+            int choice = 0;
+            while (choice != 4)
+            {
+                Console.WriteLine("1.Add Contacts");
+                Console.WriteLine("2.Edit Existing Contact");
+                Console.WriteLine("3.Delete Person Details");
+                Console.WriteLine("4.close");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter name of the address book in which you want to add record.");
+                        String bookName = Console.ReadLine();
+                        AddressBook.addBook(bookName);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the name fo the address book you want to edit.");
+                        String bookToEdit = Console.ReadLine();
+                        Console.WriteLine("Enter the first name of the person whose record you want to edit.");
+                        String recordNameToEdit = Console.ReadLine();
+                        AddressBook.editRecordUsingFisrtName(bookToEdit, recordNameToEdit);
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter name of the address book you want to delete record in.");
+                        String bookToDeleteIn = Console.ReadLine();
+                        Console.WriteLine("Enter first name of the person whose record you want to delete.");
+                        String recordNameToDelete = Console.ReadLine();
+                        AddressBook.deleteRecord(recordNameToDelete, bookToDeleteIn);
+                        break;
+                    case 4:
+                        choice = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice !");
+                        break;
+                }
+            }
 
 
 
